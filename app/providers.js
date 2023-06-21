2da5c17ee46635b63edef732751f601a589cf215
a-pitet <19183726+a-pitet@users.noreply.github.com>
2023-06-21 02:57:01 +0200

    Feature: responsive design for hero

--- app/providers.js
+++ app/providers.js
@@ -1,18 +1,27 @@
 'use client'
 
 import { CacheProvider } from '@chakra-ui/next-js'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
-// 1. Import the extendTheme function
-import { extendTheme } from '@chakra-ui/react'
-
-// 2. Extend the theme to include custom colors, fonts, etc
+// Extend the theme to include custom colors, fonts, etc
 const colors = {
   brand: {
     900: '#1a365d',
     800: '#153e75',
     700: '#2a69ac',
   },
+  'eucalyptus': '#46e0bb',
+  'keppel': '#3bbfa1',
+  'sherwood-green': '#093f32',
+  'fulvous': '#e67a04',
+  'indian-yellow': '#ee9e4e',
+  'saddle-brown': '#8e420c',
+  'grey': '#bdbdbd',
+  'vampire-black': '#0b0b0b',
+  'black-bean': '#081b17',
+  'black': '#000000',
+  'cultured': '#f6f6f6',
+  'transparent': 'transparent'
 }
   
 export const theme = extendTheme({
@@ -23,7 +32,6 @@ export const theme = extendTheme({
   },
 })  
 
-// 3. Pass the `theme` prop to the `ChakraProvider`
 export function Providers({ children }) {
   return (
     <CacheProvider>
--- components/_hero.js
+++ components/_hero.js
@@ -1,151 +1,147 @@
-'use client'
+"use client"
 
-import { Box, Heading, Image, Link, Grid, GridItem, Text, Button } from '@chakra-ui/react'
+import { Box, Button, Center, Flex, Heading, Image, Link, Text } from "@chakra-ui/react"
 
 export default function Hero() {
-    return (
-        <Grid
-            templateColumns='repeat(3, 1fr)'
-            bgGradient='linear(-10deg, #3BBFA1 -23%, #081B17 10%, #081B17 80%, #3BBFA1)'
-            justifyContent='center'
-            alignItems='center'
-            p={4}
-            borderBottomLeftRadius={14}
-            borderBottomRightRadius={14}
-        >
-            <GridItem
-                as="div"
-                colSpan={2}
-                mt={28}
-                mb={16}
-            >
-                <Image mb={4} src='../Logo.png' alt='Logo MyOwly' />
+  return (
+    <Box w="100%" bgColor="cultured" pb="5">
+      <Box w="100%" bgGradient="linear(to-b, sherwood-green, black-bean, sherwood-green)" borderBottomRadius="xl">
+        <Flex pt="10" pl="8">
+          <Box flex="1" display="flex" flexDirection={{ base: "column", md: "row" }} alignItems="center" justifyContent="space-around">
+            <Box maxWidth="500px">
+              <Image mb={4} pt="4" pr="28" src="/Logo.png" alt="Logo MyOwly"  />
+            </Box>
+            <Box pr="14">
+              <Box display={{ base: "inherit", md: "none" }}>
                 <Link
-                    href='#'
-                    textTransform="uppercase"
-                    display="inline-flex"
-                    cursor="pointer"
-                    color='white'
+                  href="#"
+                  textTransform="uppercase"
+                  display="flex"
+                  alignItems="center"
+                  cursor="pointer"
+                  color="cultured"
                 >
-                    <Image
-                        as='img'
-                        src='/arrow.svg'
-                        w={4}
-                        h={4}
-                        alt='Petite flèche'
-                        _hover={{
-                            textDecoration: "underline",
-                        }}
-                    />
-                    &nbsp;Rejoindre la Newsletter
+                  <Image
+                    as="img"
+                    src="/arrow.svg"
+                    w={3}
+                    h={3}
+                    mr="2"
+                    alt="Petite flèche"
+                  />
+                  Rejoindre la Newsletter
                 </Link>
-                <br />
-                <Link
-                    href='#'
-                    textTransform="uppercase"
-                    display="inline-flex"
-                    color='white'
-                >
-                    <Image
-                        as='img'
-                        src='/arrow.svg'
-                        w={4}
-                        h={4}
-                        alt='Petite flèche'
-                        _hover={{
-                            textDecoration: "underline",
-                        }}
-                    />
-                    &nbsp;Découvrir l&apos;équipe
-                </Link>
-            </GridItem>
-            <GridItem
+              </Box>
+              <Link
+                href="#"
+                textTransform="uppercase"
                 display="flex"
-                as="aside"
-                colSpan={1}
-                mt={2}
-                mr={-4}
-                justifyContent="right"
-            >
-                <Box
-                    backgroundColor='#3BBFA1'
-                    filter="drop-shadow(0 0 0.50rem #3BBFA1)"
-                    h="max-content"
-                    m={0}
-                    w="fit-content"
-                    p={2}
-                    display="flex"
-                    borderTopLeftRadius={8}
-                    borderBottomLeftRadius={8}
-                    flexDirection="column"
-                    position="fixed"
-                    zIndex='1'
-                >
-                    <Image as='img' src='/reseaux/discord.svg' w={30} h={30} alt='Logo Discord' mb={3} />
-                    <Image as='img' src='/reseaux/twitter.svg' w={30} h={30} alt='Logo Twitter' mb={3} />
-                    <Image as='img' src='/reseaux/reddit.svg' w={30} h={30} alt='Logo Reddit' mb={3} />
-                    <Image as='img' src='/reseaux/github-white.svg' w={30} h={30} alt='Logo Github' />
-                </Box>
-            </GridItem>
-            <GridItem
-                colSpan={3}
-                display="flex"
-                justifyContent="center"
                 alignItems="center"
-                flexDirection="row"
-                ml='auto'
-                mr='auto'
-            >
-                <Box>
-                    <Heading as="h1" textTransform="uppercase" color='white'>Soyez votre meilleur assureur</Heading>
-                    <Image as="img" src='/island.png' w="350" h="350" alt='Island 3D Model' />
-                </Box>
-                <Box>
-                    <Image as="img" src='/Cristals.png' w="160.94" h="82.77" alt='Cristals' />
-                </Box>
-            </GridItem>
-            <GridItem
-                colSpan={3}
-                display="flex"
-                justifyContent="right"
-                alignItems="right"
-                flexDirection="row"
-            >
-                <Image position='fixed' as="img" w="35" h="auto" src='/page-up.svg' alt='Page-Up Arrows' />
-            </GridItem>
-            <GridItem
-                colSpan={3}
-                display="flex"
-                justifyContent="center"
+                cursor="pointer"
+                color="cultured"
+              >
+                <Image
+                  as="img"
+                  src="/arrow.svg"
+                  w={3}
+                  h={3}
+                  mr="2"
+                  alt="Petite flèche"
+                />
+                Découvrir l&apos;équipe
+              </Link>
+            </Box>
+            <Box display={{ base: "none", md: "block" }} border="1px solid" borderColor="keppel" borderRadius="14" mr={{ base: "0", md: "4" }}>
+              <Link
+                href="#"
+                textTransform="uppercase"
                 alignItems="center"
-                flexDirection="row"
-                mb={14}
-            >
-                <Image as="img" w="16.45" h="auto" src='/arrow-white.svg' alt='Owl Arrow Down' />
-            </GridItem>
-             <GridItem
-                colSpan={3}
+                cursor="pointer"
+                color="cultured"
+                bgColor="keppel"
+                borderRadius="15"
+                border="1px solid"
+                borderColor="keppel"
+                whiteSpace="nowrap"
+                p="2"
+                boxShadow="0 0 10px var(--chakra-colors-keppel)"
+              >
+                Rejoindre la Newsletter
+              </Link>
+              <Center
+                backgroundColor="transparent"
+                m={0}
+                p="2"
                 display="flex"
-                justifyContent="center"
-                alignItems="center"
-                textAlign="center"
-                flexDirection="column"
-                ml="auto"
-                mr="auto"
+                flexDirection="row"
+              >
+                <Link href="https://app.myowly.com"><Image as="img" src="/reseaux/discord.svg" w="30" h="30" alt="Logo Discord" m="3" /></Link>
+                <Link href="https://app.myowly.com"><Image as="img" src="/reseaux/twitter.svg" w="30" h="30" alt="Logo Twitter" m="3" /></Link>
+                <Link href="https://app.myowly.com"><Image as="img" src="/reseaux/reddit.svg" w="30" h="30" alt="Logo Reddit" m="3" /></Link>
+                <Link href="https://app.myowly.com"><Image as="img" src="/reseaux/github-white.svg" w="30" h="30" alt="Logo Github" m="3" /></Link>
+              </Center>
+            </Box>
+          </Box>
+          <Box display={{ base: "inherit", md: "none" }}>
+            <Center
+              backgroundColor="keppel"
+              m={0}
+              p="2"
+              display="flex"
+              borderStartRadius={14}
+              boxShadow="0 0 10px var(--chakra-colors-keppel)"
+              flexDirection="column"
+              zIndex="1"
             >
-                <Heading mb={4} as="h2" textTransform="uppercase" color='orange'>La Blockchain révolutionne l&apos;assurance</Heading>
-                <Text textAlign='justify' mb={12}>Que vous cherchiez à vous protéger contre les risques naturels, les pertes financières ou toute autres situations imprévue, notre assurance paramétrique est là pour vous aider.</Text>
-                <Button background="transparent" mb={4} border="1px solid #3BBFA1" borderRadius={16} w="100%" justifyContent="space-between">
-                    <Image as="img" w="auto" h="27" src='/bec.svg' alt='Owl Bec' />
-                    <Text as="p" color="#3BBFA1" textTransform="uppercase" fontWeight="bold">&nbsp;&nbsp;En savoir plus&nbsp;&nbsp;</Text>
-                    <Image as="img" w="auto" h="27" src='/bec.svg' alt='Owl Bec' />
-                </Button>
-                <Button mb='6rem' background="transparent" border="1px solid #3BBFA1" borderRadius={16} w="100%" justifyContent="space-between">
-                    <Image as="img" w="auto" h="27" src='/bec.svg' alt='Owl Bec' />
-                    <Text as="p" color="#3BBFA1" textTransform="uppercase" fontWeight="bold">&nbsp;&nbsp;Se tenir informé&nbsp;&nbsp;</Text>
-                    <Image as="img" w="auto" h="27" src='/bec.svg' alt='Owl Bec' />
-                </Button>
-            </GridItem>
-        </Grid>
-    )
+              <Link href="https://app.myowly.com"><Image as="img" src="/reseaux/discord.svg" w="30" h="30" alt="Logo Discord" mb="3" /></Link>
+              <Link href="https://app.myowly.com"><Image as="img" src="/reseaux/twitter.svg" w="30" h="30" alt="Logo Twitter" mb="3" /></Link>
+              <Link href="https://app.myowly.com"><Image as="img" src="/reseaux/reddit.svg" w="30" h="30" alt="Logo Reddit" mb="3" /></Link>
+              <Link href="https://app.myowly.com"><Image as="img" src="/reseaux/github-white.svg" w="30" h="30" alt="Logo Github" /></Link>
+            </Center>
+          </Box>
+        </Flex>
+        
+        <Center pt="8" px="8" display="flex" flexDirection={{ base: "column", md: "row" }}>
+          <Heading as="h1" textTransform="uppercase" fontSize="5xl" color="cultured" lineHeight="1">
+            Soyez
+            <Text as="span" color="sherwood-green" textShadow="-1px 0 white, 0 1px white, 1px 0 white, 0 -1px white"> votre </Text><br/>
+            meilleur<br/>
+            <Text as="span" color="sherwood-green" textShadow="-1px 0 white, 0 1px white, 1px 0 white, 0 -1px white"> assureur </Text>
+          </Heading>
+          <Box display="flex">
+            <Image as="img" src="/island.png" w="350" h="350" alt="Island 3D Model" transform={{ base: "translateY(-50px);", md: "translateY(30px)" }} />
+            <Image as="img" src="/Cristals.png" w="320" h="165" alt="Cristals" transform={{ base: "translate(-60px, -80px);", md: "translate(-400px, -20px) rotate(0.80turn)" }} />
+          </Box>
+        </Center>
+        
+        <Box display="flex" justifyContent="center" pt={{ base: "0", md: "14" }} pb="14">
+          <Image as="img" w="16.45" h="auto" src="/arrow-white.svg" alt="Owl Arrow Down" />
+        </Box>    
+
+        <Box px="8" maxWidth="500px" m="auto">
+          <Heading as="h2" mb={4} textTransform="uppercase" lineHeight="1">
+            <Text as="span" color="cultured" fontSize="4xl"> La Blockchain </Text>
+            <Text as="span"color="indian-yellow" fontSize="4xl"> révolutionne </Text>
+            <Text as="span"color="fulvous" fontSize="5xl"> l&apos;assurance </Text>
+          </Heading>
+          
+          <Text mb={12} color="cultured">
+            Que vous cherchiez à vous protéger contre les risques naturels, les pertes financières ou toute autres situations imprévue, notre assurance paramétrique est là pour vous aider.
+          </Text>
+
+          <Button background="transparent" mb={4} border="1px solid" borderColor="keppel" borderRadius={16} w="100%" justifyContent="space-between" boxShadow="0 0 10px var(--chakra-colors-keppel)">
+            <Image as="img" w="auto" h="27" src="/bec.svg" alt="Owl Bec" />
+            <Text as="p" color="keppel" textTransform="uppercase" fontWeight="bold">En savoir plus</Text>
+            <Image as="img" w="auto" h="27" src="/bec.svg" alt="Owl Bec" />
+          </Button>
+
+          <Button mb="6rem" background="transparent" border="1px solid" borderColor="keppel" borderRadius={16} w="100%" justifyContent="space-between" boxShadow="0 0 10px var(--chakra-colors-keppel)">
+            <Image as="img" w="auto" h="27" src="/bec.svg" alt="Owl Bec" />
+            <Text as="p" color="keppel" textTransform="uppercase" fontWeight="bold">Se tenir informé</Text>
+            <Image as="img" w="auto" h="27" src="/bec.svg" alt="Owl Bec" />
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  )
 }
