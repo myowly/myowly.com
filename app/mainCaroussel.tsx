c2b730cdb95236dd935ac8147011f34fe1380f3b
Thomas Castellengo <thomas@myowly.com>
2023-06-22 17:38:35 +0200

    Feature(ui): modal on carousel

--- app/mainCaroussel.tsx
+++ app/mainCaroussel.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { Box, Button, Flex, Heading, IconButton, Icon, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, IconButton, Icon, Text, useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter } from "@chakra-ui/react";
 import ArrowBlack from "@/svg/arrow-black";
 import ArrowWhite from "@/svg/arrow-white";
 import Beak from '@/svg/beak';
+import { slides } from './slides';
 
 export default function MainCaroussel() {
 	const arrowStyles = {
@@ -20,62 +21,6 @@ export default function MainCaroussel() {
 		bgColor: "transparent"
 	} as const;
 
-	const slides = [
-		{
-			img: "/backgrounds/field.webp",
-			title: (
-				<>
-					<Box as="span" color="vampire-black">Une </Box>
-					<Box as="span" color="indian-yellow">assurance</Box> paramétri&shy;que
-				</>
-			),
-			subtitle: (
-				<>
-					Tout est <Box as="span" color="spanish-orange" fontStyle="italic">prévu </Box><wbr />
-					pour <Box as="span" color="spanish-orange" fontStyle="italic"> l’imprévu</Box>
-				</>
-			),
-			description: "Une indemnisation plus rapide et plus efficace et sans avoir à prouver les pertes subies.",
-			mode: "ligth"
-		},
-		{
-			img: "/backgrounds/cube.webp",
-			title: (<><Box as="span" color="spanish-orange">La</Box> Dao</>),
-			subtitle: (<>
-				La solution d’assurance <Box as="span" color="fulvous">du </Box>
-				<Box as="span" color="fulvous">futur</Box>
-			</>),
-			description: "Une pool de liquidités et des couvertures autogérées par la communauté.",
-			mode: "dark"
-		},
-		{
-			img: "/backgrounds/oracle.webp",
-			title: (<><Box as="span" color="indian-yellow" >L’oracle</Box> MYOWLY</>),
-			subtitle: (
-				<>
-					La <Box as="span" color="indian-yellow" >fiabilité</Box> des
-					<Box as="span" color="spanish-orange"> contrats intelligents</Box>
-				</>
-			),
-			description: "Des sources de données fiables utilisées pour déclencher les smart contracts.",
-			mode: "dark"
-		},
-		{
-			img: "/backgrounds/ia.webp",
-			title: (
-				<>
-					<Box as="span" color="vampire-black">Une</Box> <Box as="span" color="indian-yellow">IA</Box> ACTUARIELLE
-
-				</>),
-			subtitle: (
-				<>La <Box as="span" color="indian-yellow">précision</Box>,
-					pour <Box as="span" color="spanish-orange">un avenir sûr</Box>
-				</>),
-			description: "L'intelligence artificielle  analyse des données complexes et prévoit les risques futurs avec une précision accrue.",
-			mode: "ligth"
-		},
-	];
-
 	const [currentSlide, setCurrentSlide] = useState(0);
 
 	const slidesCount = slides.length;
@@ -90,7 +35,120 @@ export default function MainCaroussel() {
 		setCurrentSlide(slide);
 	};
 	const carouselSlice = {
-		ml: `-${currentSlide * 100}%`,
+		ml: [
+			`-${currentSlide * 100}%`,
+			null,
+			`-${currentSlide * 50}%`,
+			`-${currentSlide * 45}%`,
+			`-${currentSlide * 33}%`
+		],
+	};
+
+	const Slide = ({ slide, sid }) => {
+		const { isOpen, onOpen, onClose } = useDisclosure();
+		return (
+			<Box
+				boxSize="full"
+				flex="none"
+				{...(sid === 0 ? { ml: [null, null, '40%'] } : {})}
+				mr={[null, null, '50px', '70px']}
+			>
+				<Flex
+					direction="column"
+					justifyContent="center"
+					alignItems="center"
+					w="full"
+					h="100vh"
+					pt={['20px', '100px', 0]}
+					px={['22px', '15.5%', 0]}
+					pb={['20px', '100px', 0]}
+				>
+					<Flex
+						direction="column"
+						justifyContent="center"
+						maxWidth="460px"
+						{...(currentSlide === sid ? {
+							py: ['14%', '78px', '78px'],
+						} : {
+							py: ['14%', '78px', '40px'],
+						})}
+						px={['14%', '54px']}
+						transition="all .5s"
+						backdropFilter="blur(10px)"
+						border="1px solid"
+						borderColor={slide.mode === 'ligth' ? 'vampire-black' : 'cultured'}
+						borderRadius="56px"
+						bgGradient="linear(110.72deg, rgba(255, 255, 255, 0.36) 1.21%, rgba(196, 196, 196, 0.06) 100%);"
+						{...(currentSlide < sid ? {
+							onClick: nextSlide,
+							cursor: "pointer"
+						} : currentSlide > sid ? {
+							onClick: prevSlide,
+							cursor: "pointer"
+						} : {}
+						)}
+					>
+						<Heading
+							as="h3"
+							size="2xl"
+							color="fulvous"
+							textTransform="uppercase"
+							fontSize={['43px', '50px']}
+							lineHeight={['43px', '50px']}
+							fontWeight="normal"
+						>
+							{slide.title}
+						</Heading>
+						<Heading
+							as="h4"
+							mt={['35px', '50px', '100px']}
+							fontFamily="body"
+							color={slide.mode == 'ligth' ? "vampire-black" : "cultured"}
+							textTransform="uppercase"
+							fontWeight="600"
+							fontSize={['24px', '25px']}
+							lineHeight={['24px', '25px']}
+						>
+							{slide.subtitle}
+						</Heading>
+						<Text
+							color={slide.mode == 'ligth' ? "vampire-black" : "cultured"}
+							mt="20px"
+						>{slide.description}</Text>
+						<Button
+							mt={['30px', null, '55px']}
+							textTransform="uppercase"
+							bgColor="cultured"
+							color="keppel"
+							p="0px 40px"
+							borderRadius="20px"
+							fontSize="20px"
+							lineHeight="26px"
+							fontWeight="700"
+							onClick={(e) => { e.stopPropagation(); onOpen() }}
+						>
+							En savoir plus
+						</Button>
+					</Flex>
+				</Flex>
+				<Modal onClose={onClose} isOpen={isOpen} isCentered>
+					<ModalOverlay backdropBlur="2px" />
+					<ModalContent bgColor="cultured" padding="50px 20px">
+						<ModalHeader
+							textTransform="uppercase"
+							color="keppel"
+							fontSize="50px"
+							lineHeight="50px"
+						>{slide.modalHeading}</ModalHeader>
+						<ModalCloseButton color="vampire-black" />
+						<ModalBody>
+							{slide.modalBody}
+						</ModalBody>
+					</ModalContent>
+				</Modal>
+			</Box>
+		);
+
 	};
 
 	return (
@@ -112,80 +170,16 @@ export default function MainCaroussel() {
 				bgPosition="center"
 			>
 				<Flex
-					w="full"
+					w={['100%', null, '50%', '45%', '33%']}
 					transition="all .5s"
 					{...carouselSlice}
 				>
 					{slides.map((slide, sid) => (
-						<Box key={`slide-${sid}`} boxSize="full" shadow="md" flex="none">
-							<Flex
-								direction="column"
-								justifyContent="center"
-								alignItems="center"
-								w="full"
-								h="100vh"
-								pt={['20px', '100px']}
-								px={['22px', '15.5%']}
-								pb={['20px', '100px']}
-							>
-								<Flex
-									direction="column"
-									justifyContent="center"
-									maxWidth="460px"
-									py={['14%', '78px']}
-									px={['14%', '54px']}
-									backdropFilter="blur(10px)"
-									border="1px solid"
-									borderColor={slide.mode === 'ligth' ? 'vampire-black' : 'cultured'}
-									borderRadius="56px"
-									bgGradient="linear(110.72deg, rgba(255, 255, 255, 0.36) 1.21%, rgba(196, 196, 196, 0.06) 100%);"
-								>
-									<Heading
-										as="h3"
-										size="2xl"
-										color="fulvous"
-										textTransform="uppercase"
-										fontSize={['43px', '50px']}
-										lineHeight={['43px', '50px']}
-										fontWeight="normal"
-									>
-										{slide.title}
-									</Heading>
-									<Heading
-										as="h4"
-										mt={['35px', '50px', '100px']}
-										fontFamily="body"
-										color={slide.mode == 'ligth' ? "vampire-black" : "cultured"}
-										textTransform="uppercase"
-										fontWeight="600"
-										fontSize={['24px', '25px']}
-										lineHeight={['24px', '25px']}
-									>
-										{slide.subtitle}
-									</Heading>
-									<Text
-										color={slide.mode == 'ligth' ? "vampire-black" : "cultured"}
-										mt="20px"
-									>{slide.description}</Text>
-									<Button
-										mt={['30px', null, '55px']}
-										textTransform="uppercase"
-										bgColor="cultured"
-										color="keppel"
-										p="0px 40px"
-										borderRadius="20px"
-										fontSize="20px"
-										lineHeight="26px"
-										fontWeight="700"
-									>
-										En savoir plus
-									</Button>
-								</Flex>
-							</Flex>
-						</Box>
+						<Slide slide={slide} sid={sid} key={`slide-${sid}`} />
 					))}
 				</Flex>
 				<IconButton
+					display={[null, null, 'none']}
 					left="0"
 					onClick={prevSlide}
 					transform="rotate(90deg)"
@@ -195,6 +189,7 @@ export default function MainCaroussel() {
 					{...arrowStyles}
 				/>
 				<IconButton
+					display={[null, null, 'none']}
 					right="0"
 					onClick={nextSlide}
 					transform="rotate(-90deg)"
