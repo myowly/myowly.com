0c2cceaa4106928eb8c6d55c3df58fd86ecff865
Thomas Castellengo <thomas@myowly.com>
2023-06-22 11:28:40 +0200

    Fix(style): remove global.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,18 @@
-import './globals.css'
 import { Providers } from "./providers";
 
 export const metadata = {
-  title: 'MyOwly',
-  description: 'MyOwly',
+	title: 'MyOwly',
+	description: 'MyOwly',
 }
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <Providers>
-          {children}
-        </Providers>
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body>
+				<Providers>
+					{children}
+				</Providers>
+			</body>
+		</html>
+	)
 }
