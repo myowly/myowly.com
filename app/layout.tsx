ffaf09a4b3fabdaea218935883874fef39c0105f
a-pitet <19183726+a-pitet@users.noreply.github.com>
2023-06-25 21:13:29 +0200

    Feature: Hubspot tracking code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,22 @@
+import Head from "next/head";
 import { Providers } from "./providers";
 
 export const metadata = {
-	title: 'MyOwly',
-	description: 'MyOwly',
+  title: 'MyOwly',
+  description: 'MyOwly',
 }
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en">
-			<body>
-				<Providers>
-					{children}
-				</Providers>
-			</body>
-		</html>
-	)
+  return (
+    <html lang="en">
+      <Head>
+        <script type="text/javascript" id="hs-script-loader" async defer src="//js-eu1.hs-scripts.com/139772533.js"></script>
+      </Head>
+      <body>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  )
 }
